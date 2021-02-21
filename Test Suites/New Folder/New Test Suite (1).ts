3da34c0b4e656cf4c1dd2924a27bbbcb1c252369
pavan <pavan.satyala@fisclouds.com>
2021-02-21 17:49:43 +0530

    ss commited

---
+++ Test Suites/New Folder/New Test Suite (1).ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite (1)</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>74da13f8-907e-4a7c-8afa-8e35075a97db</testSuiteGuid>
+</TestSuiteEntity>
